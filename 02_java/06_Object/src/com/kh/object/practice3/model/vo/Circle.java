55166df3f70141d8aebca7c24a7e248ec2f82280
hyeyoung <roenv9@gmail.com>
2025-02-06 20:05:36 +0900

    Circle.java を更新

--- 02_java/06_Object/src/com/kh/object/practice3/model/vo/Circle.java
+++ 02_java/06_Object/src/com/kh/object/practice3/model/vo/Circle.java
@@ -3,7 +3,7 @@ package com.kh.object.practice3.model.vo;
 public class Circle {
 
 	// 필드
-	private static double PI = 3.14;
+	private static final double PI = 3.14;
 	private static int radius = 1;
 	
 	// 생성
